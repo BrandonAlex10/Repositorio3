01218df474dfa90fa3ea75229068a59c7b966be0
SOPORTE-01 <SOPORTE-01@SOPORTE02-NESTL>
2017-07-10 13:11:46 -0500

    Modificacion De Clase 13:11 - Usuario2

---
+++ AplicacionGIT/AplicacionGIT/Models/NewClase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AplicacionGIT.Models
+{
+    public class NewClase
+    {
+
+        public double Sueldo { get; set; }
+
+    }
+}
