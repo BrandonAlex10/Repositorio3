e049bdcf796d9921b14c4f02ac7f36dcbbd9b880
SOPORTE-01 <SOPORTE-01@SOPORTE02-NESTL>
2017-07-10 11:40:17 -0500

    solucion 3

---
+++ AplicacionGIT/AplicacionGIT/Models/kkk.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AplicacionGIT.Models
+{
+    public class kkk
+    {
+        public int numero { get; set; }
+    }
+}
